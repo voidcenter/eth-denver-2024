4f0c37953b2333104fe9908f6bc9ae43c37a4ad1
Justin <justin@Justins-MacBook-Pro-2.local>
2024-03-02 08:32:23 -0700

    rm

--- src/app/components/steps/BusinessLogic.tsx
+++
@@ -1,7 +0,0 @@
-export default function BusinessLogic() {
-    return (
-        <div className="business-logic h-full flex flex-col justify-between border border-common-gray rounded-[15px] shadow-sm">
-
-        </div>
-    );
-}
--- src/app/components/util/DropDownMenu.tsx
+++
@@ -1,7 +0,0 @@
-export default function DeopDownMenu() {
-    return (
-        <div className="dropdown">
-
-        </div>
-    );
-}
